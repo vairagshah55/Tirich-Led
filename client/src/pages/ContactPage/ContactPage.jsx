099e3f35fab0bd5577f5fde2bb2f04dc97c2d43c
vairag shah <vairagshah55@gmail.com>
2026-07-11 08:56:59 +0530

    contact us page changes

--- client/src/pages/ContactPage/ContactPage.jsx
+++ client/src/pages/ContactPage/ContactPage.jsx
@@ -16,14 +16,14 @@ const CONTACT_INFO = [
   {
     icon: <><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></>,
     label: 'Email',
-    value: 'info@tirichled.com',
-    href: 'mailto:info@tirichled.com',
+    value: 'salestirichled@gmail.com',
+    href: 'mailto:salestirichled@gmail.com',
   },
   {
     icon: <><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></>,
     label: 'Phone',
-    value: '+91 98765 43210',
-    href: 'tel:+919876543210',
+    value: '+91 73832 47625',
+    href: 'tel:+917383247625',
   },
   {
     icon: <><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></>,
